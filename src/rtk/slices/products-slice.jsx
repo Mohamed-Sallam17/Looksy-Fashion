c2197538b9d8ceaa1c43f4557fba0f7337346cba
unknown <mohamedsallam>
2026-07-08 16:57:03 +0300

    first commit

---
+++ src/rtk/slices/products-slice.jsx
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+    name:'productSlice',
+    initialState: [],
+    reducers:{
+        
+    }
+})
+
+export default productSlice.reducer
