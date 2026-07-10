58982f7ce583d20112d501b73efb5c44d2d9ab7c
mohamedSallam <wms25833@gmail.com>
2026-07-10 04:24:32 +0300

    work on hero banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom"
+import heroImg from '../assets/hero-img.webp'
 
 function Hero() {
   return (
-    <section className="hero">
-      <div className="container">
-        <div className="flex">
-          <div className="hero__content">
-            <h2>
-              LOOK <br />
-              GOOD <br />
-              FEEL <br />
-              DIFFERENT.
-            </h2>
-            <p>
-              At Looksy, we blend timeless style with modern attitude — made for those who dare to be different.
-            </p>
-            <Link to="/shop">
-              <button> Shop Now </button>
-            </Link>
+    <section className="hero h-[100dvh]">
+      <div className="flex h-full gap-4">
+        <div className="hero__content flex-1 space-y-4 pl-6">
+          <h2 className="text-2xl lg:text-5xl leading-[2em] ">
+            LOOK <br />
+            GOOD <br />
+            FEEL <br />
+            DIFFERENT.
+          </h2>
+          <p className="text-[var(--sub-color)]">
+            At Looksy, we blend timeless style with modern attitude — made for those who dare to be different.
+          </p>
+          <Link to="/shop" className="w-[140px] h-[56px] bg-[var(--btn-color)] flex justify-center mt-10">
+            <button className="text-bold"> Shop Now </button>
+          </Link>
+        </div>
+        <div className="hero__media flex-1 bg-[var(--bg-color)] pr-6">
+          <div className="hero_model h-full">
+            <img src={heroImg} alt="Hero Banner" className="w-[50%] h-full" />
           </div>
-          <div className="hero__media"></div>
         </div>
       </div>
     </section>
